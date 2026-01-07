fcc4a40f9c45ac7130d82418053d69ad63737c9d
Dema81 <dema.solutions.cloud@gmail.com>
2026-01-07 01:44:00 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,19 +1,3 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-
-console.log("React is bootstrapping...");
-
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-  console.log("React render initiated.");
-} else {
-  console.error("Root element not found!");
-}
+// Contenuto rimosso per stabilità su GitHub Pages.
+// Il bootstrap è gestito direttamente in index.html.
